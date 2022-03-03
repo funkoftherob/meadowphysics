5bd1bd15d79e9cec05ac7f1428017474ed81f67c
funkoftherob <funkoftherob@gmail.com>
2022-03-03 18:24:18 -0500

    Update mxsynths.lua

--- mxsynths.lua
+++ mxsynths.lua
@@ -18,63 +18,24 @@ local hs = include('lib/halfsecond')
 
 g = grid.connect()
 
-engine.name = 'PolyPerc'
+engine.name="MxSynths"
+local mxsynths_=include("mx.synths/lib/mx.synths")
+mxsynths=mxsynths_:new()
+
 
 -- voicing
 
 function trigger(note_num, hz, voice)
-  engine.hz(hz)
+  engine.mx_note_on(note_num,1,60)
 end
 
 function gate_high(note_num, hz, voice)
-  engine.hz(hz)
+  engine.mx_note_on(note_num,1,60)
 end
 
 function gate_low(note_num, hz, voice)
-  engine.hz(hz)
+  engine.mx_note_on(note_num,1,60)
 end
-
-
-function init_engine ()
-  params:add_separator()
-  cs_AMP = controlspec.new(0,1,'lin',0,0.5,'')
-  params:add{
-    type="control",id="amp",controlspec=cs_AMP,
-    action=function(x) engine.amp(x) end
-  }
-
-  cs_PW = controlspec.new(0,100,'lin',0,50,'%')
-  params:add{
-    type="control",id="pw",controlspec=cs_PW,
-    action=function(x) engine.pw(x/100) end
-  }
-
-  cs_REL = controlspec.new(0.1,3.2,'lin',0,1.2,'s')
-  params:add{
-    type="control",id="release",controlspec=cs_REL,
-    action=function(x) engine.release(x) end
-  }
-
-  cs_CUT = controlspec.new(50,5000,'exp',0,800,'hz')
-  params:add{
-    type="control",id="cutoff",controlspec=cs_CUT,
-    action=function(x) engine.cutoff(x) end
-  }
-
-  cs_GAIN = controlspec.new(0,4,'lin',0,1,'')
-  params:add{
-    type="control",id="gain",controlspec=cs_GAIN,
-    action=function(x) engine.gain(x) end
-  }
-  
-  cs_PAN = controlspec.new(-1,1, 'lin',0,0,'')
-  params:add{
-    type="control",id="pan",controlspec=cs_PAN,
-    action=function(x) engine.pan(x) end
-  }
-  hs.init()
-end
-
 
 
 
@@ -82,7 +43,6 @@ end
 
 function init()
   meadowphysics.init()
-  init_engine()
 end
 
 function key(n,z)
